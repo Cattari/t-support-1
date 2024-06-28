5c5cd7cf79c5f388fc1e15c4d60708af4aaee344
Andrii Shyrokopoias <Andrii_Shyrokopoais@epam.com>
2024-06-29 02:57:29 +0300

    Fix

--- handlers.py
+++ handlers.py
@@ -64,7 +64,7 @@ async def forward_to_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
     }"""
     user_id = None
     print(update.message.reply_to_message)
-    if update.message.reply_to_message.from_user:
+    if update.message.reply_to_message.from_user.id:
         user_id = update.message.reply_to_message.from_user.id
     elif REPLY_TO_THIS_MESSAGE in update.message.reply_to_message.text:
         try:
