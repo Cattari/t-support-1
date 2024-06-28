b5ec4a672487a7e1eadaa09685f1d008733ca710
Andrii Shyrokopoias <Andrii_Shyrokopoais@epam.com>
2024-06-29 01:48:11 +0300

    FIx

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from telegram.ext import Application
 from telegram import Update
 
-import asyncio
+import uvicorn
 from http import HTTPStatus
 from contextlib import asynccontextmanager
 from handlers import setup_dispatcher
@@ -9,43 +9,42 @@ from settings import TELEGRAM_TOKEN, HEROKU_APP_NAME, PORT
 
 from fastapi import FastAPI, Request, Response
 
-async def main():
-    print(f"Running bot in webhook mode. Make sure that this url is correct: https://{HEROKU_APP_NAME}.herokuapp.com/")
+print(f"Running bot in webhook mode. Make sure that this url is correct: https://{HEROKU_APP_NAME}.herokuapp.com/")
 
-    ptb = (
-        Application.builder()
-            .updater(None)
-            .token(TELEGRAM_TOKEN) # replace <your-bot-token>
-            .read_timeout(7)
-            .get_updates_read_timeout(42)
-            .build()
-    )
+ptb = (
+    Application.builder()
+        .updater(None)
+        .token(TELEGRAM_TOKEN) # replace <your-bot-token>
+        .read_timeout(7)
+        .get_updates_read_timeout(42)
+        .build()
+)
 
-    @asynccontextmanager
-    async def lifespan(_: FastAPI):
-        await ptb.bot.setWebhook(f"https://{HEROKU_APP_NAME}.herokuapp.com/{TELEGRAM_TOKEN}") # replace <your-webhook-url>
-        async with ptb:
-            await ptb.start()
-            yield
-            await ptb.stop()
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    await ptb.bot.setWebhook(f"https://{HEROKU_APP_NAME}.herokuapp.com/{TELEGRAM_TOKEN}") # replace <your-webhook-url>
+    async with ptb:
+        await ptb.start()
+        yield
+        await ptb.stop()
 
-    # Initialize FastAPI app (similar to Flask)
-    app = FastAPI(lifespan=lifespan)
+# Initialize FastAPI app (similar to Flask)
+app = FastAPI(lifespan=lifespan)
 
-    @app.post("/telegram")
-    async def process_update(request: Request):
-        req = await request.json()
-        update = Update.de_json(req, ptb.bot)
-        await ptb.process_update(update)
-        return Response(status_code=HTTPStatus.OK)
+@app.post("/telegram")
+async def process_update(request: Request):
+    req = await request.json()
+    update = Update.de_json(req, ptb.bot)
+    await ptb.process_update(update)
+    return Response(status_code=HTTPStatus.OK)
 
-    @app.get("/hc")  # type: ignore[misc]
-    async def health(request: Request) -> Response:
-        """For the health endpoint, reply with a simple plain text message."""
-        return Response(status_code=HTTPStatus.OK, content="OK")
-    
-    setup_dispatcher(ptb)
-    
+@app.get("/hc")  # type: ignore[misc]
+async def health(request: Request) -> Response:
+    """For the health endpoint, reply with a simple plain text message."""
+    return Response(status_code=HTTPStatus.OK, content="OK")
+
+setup_dispatcher(ptb)
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    uvicorn.run("main:app")
 
