eb34675705573687794a95aa12af50a50dd2bf83
Andrii Shyrokopoias <Andrii_Shyrokopoais@epam.com>
2024-06-29 02:04:30 +0300

    FIx

--- main.py
+++ main.py
@@ -46,5 +46,5 @@ async def health(request: Request) -> Response:
 setup_dispatcher(ptb)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=PORT)
+    uvicorn.run("main:app", host="0.0.0.0", port=PORT)
 
