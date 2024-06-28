1a9503d3aee536aa33f2697f30195d79e0527916
Andrii Shyrokopoias <Andrii_Shyrokopoais@epam.com>
2024-06-29 00:03:23 +0300

    Fix

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ def main():
         .token(TELEGRAM_TOKEN)
         .build()
     )
+
+    application.run_webhook(
+        listen="0.0.0.0",                          
+        port=int(PORT),                          
+        webhook_url='https://{HEROKU_APP_NAME}.herokuapp.com/{TELEGRAM_TOKEN}'
+    )
     setup_dispatcher(application)
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
