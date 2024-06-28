5e97f62b86b8ff07fc503b8b01b437ce6e433716
Andrii Shyrokopoias <Andrii_Shyrokopoais@epam.com>
2024-06-29 00:22:19 +0300

    Fix

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
-from telegram.ext import Updater, Application
 from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
 
-import asyncio
 from handlers import setup_dispatcher
 from settings import TELEGRAM_TOKEN, HEROKU_APP_NAME, PORT
 
 def main():
     print(f"Running bot in webhook mode. Make sure that this url is correct: https://{HEROKU_APP_NAME}.herokuapp.com/")
 
-    application = (
-        Application.builder()
-        .token(TELEGRAM_TOKEN)
-        .build()
+    updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
+
+    setup_dispatcher(updater)
+
+    updater.start_webhook(
+        listen="0.0.0.0",
+        port=PORT,
+        url_path=TELEGRAM_TOKEN
     )
-
-    setup_dispatcher(application)
-
-    application.run_webhook(
-        listen="0.0.0.0",                          
-        port=int(PORT),                          
-        webhook_url=f"https://{HEROKU_APP_NAME}.herokuapp.com/{TELEGRAM_TOKEN}"
-    )
-
-    application.updater.start_polling()
+    updater.bot.set_webhook(f"https://{HEROKU_APP_NAME}.herokuapp.com/{TELEGRAM_TOKEN}".format(HEROKU_APP_NAME, TOKEN))
 
 if __name__ == "__main__":
     main()
