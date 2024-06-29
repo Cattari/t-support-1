dff9842506ec5e5f243c5bfaff866ca5414a54fc
Andrii Shyrokopoias <Andrii_Shyrokopoais@epam.com>
2024-06-29 18:31:54 +0300

    Cache added

--- handlers.py
+++ handlers.py
@@ -6,6 +6,10 @@ from settings import (
     WELCOME_MESSAGE, 
     TELEGRAM_SUPPORT_CHAT_ID,
 )
+from cachetools import TTLCache
+
+bot_forward_data = TTLCache(maxsize=1000, ttl=60*60*24*2)
+"""message_id - user_id; cache time for each key/message - 2 days"""
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(WELCOME_MESSAGE)
@@ -19,8 +23,6 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         """,
     )
 
-bot_forward_data = {}
-
 async def ban(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(BAN_MESSAGE)
 
@@ -28,8 +30,6 @@ async def ban(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     if bot_forward_data[update.message.reply_to_message.id]:
         user_id = bot_forward_data[update.message.reply_to_message.id]
-
-    # user_info = update.message.from_user.to_dict()
 
     await context.bot.send_message(
         chat_id=user_id,
@@ -51,18 +51,13 @@ async def forward_to_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if bot_forward_data[update.message.reply_to_message.id]:
         user_id = bot_forward_data[update.message.reply_to_message.id]
     is_reply_to_forwarded_by_bot = update.message.reply_to_message.from_user.is_bot
-    
-    print('REPLY TO MESSAGE')
-    print(update.message.reply_to_message)
     if user_id and is_reply_to_forwarded_by_bot:
         await context.bot.copy_message(
             message_id=update.message.message_id,
             chat_id=user_id,
             from_chat_id=update.message.chat_id
         )
-        del bot_forward_data[update.message.reply_to_message.id]
-        print('BOT FORWARD DATA')
-        print(bot_forward_data)
+
 
 def setup_dispatcher(application: Application):
     application.add_handler(CommandHandler('start', start))
