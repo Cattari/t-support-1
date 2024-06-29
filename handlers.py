0b2d6441dd2089b1b693047f9903c397f56075a2
Andrii Shyrokopoias <Andrii_Shyrokopoais@epam.com>
2024-06-29 18:54:55 +0300

    Test

--- handlers.py
+++ handlers.py
@@ -25,11 +25,10 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 async def ban(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print('REPLY TO MESSAGE')
-    print(update.message.reply_to_message)
-    # banned_user_name = update.message.reply_to_message
-    await update.message.reply_text(
-        BAN_MESSAGE_ADMIN.format(user_name='test user name')
+    banned_user_name = update.message.reply_to_message.sender_user_name
+    await context.bot.send_message(
+        chat_id=TELEGRAM_SUPPORT_CHAT_ID,
+        text=BAN_MESSAGE_ADMIN.format(user_name=banned_user_name),
     )
 
     user_id = None
