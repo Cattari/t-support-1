f71ee0c9b68e6581554ae271e180fee179a46bec
Andrii Shyrokopoias <Andrii_Shyrokopoais@epam.com>
2024-06-29 13:04:14 +0300

    Test

--- handlers.py
+++ handlers.py
@@ -63,6 +63,10 @@ async def forward_to_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
         'from': {'id': 49820636, 'first_name': 'Daniil', 'is_bot': False, 'last_name': 'Okhlopkov', 'username': 'danokhlopkov', 'language_code': 'en'}
     }"""
     user_id = None
+    print('REPLY TO USER')
+    print(update.message.from_user)
+    print('REPLY TO MESSAGE')
+    print(update.message.reply_to_message)
     if update.message.from_user:
         user_id = update.message.from_user.id
     if user_id:
